bda911e758fe9ddf025469fedb40869c707631b9
shinee20 <ujmikzz7@daum.net>
2022-01-26 17:31:56 +0900

    init

---
+++ src/main/java/com/avocado/pizziochat/PizzioChatApplication.java
@@ -0,0 +1,13 @@
+package com.avocado.pizziochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzioChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzioChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avocado/pizziochat/PizzioChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.pizziochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzioChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
