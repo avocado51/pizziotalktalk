5f2e31cea95166d29360f21444043be50b7bffe2
shinee20 <ujmikzz7@hanmail.net>
2022-02-03 21:29:22 +0900

    CHANGE test package name

--- src/test/java/com/avocado/pizziotalktalk/PizzioTalkTalkApplicationTests.java
+++ src/test/java/com/avocado/pizziotalktalk/PizzioTalkTalkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.avocado.pizziochat;
+package com.avocado.pizziotalktalk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PizzioChatApplicationTests {
+class PizzioTalkTalkApplicationTests {
 
 	@Test
 	void contextLoads() {
